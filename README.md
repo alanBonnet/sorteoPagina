085b628d25ed3fa45fe3b675773773c9f8fb49a3
alanBonnet <84151943+alanBonnet@users.noreply.github.com>
2021-09-10 02:33:21 -0300

    pagina sorteado

---
+++ README.md
@@ -0,0 +1,3 @@
+# sorteoPagina
+una pagina para sortear temas o nombres de personas, primera vez que lo hago.
+ https://alanbonnet.github.io/sorteoPagina/
---
+++ js/hola.js
@@ -0,0 +1,8 @@
+// listen-sv
+document.querySelector('#agregar').addEventListener('click', agregar);
+let nombres = [];
+function agregar(){
+    console.log("se ejecutó")
+    let nombre = document.querySelector('#nombre').value;
+    console.log(nombre)
+}
